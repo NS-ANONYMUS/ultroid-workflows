2a87e1be5af6ece46e390feec71225424e4fb320
NS-ANONYMUS <87913206+NS-ANONYMUS@users.noreply.github.com>
2021-08-30 04:01:50 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,6 +1,6 @@
 branch=main
 git clone -b $branch https://github.com/TeamUltroid/Ultroid /root/TeamUltroid
-cp ultroid/.env /root/TeamUltroid/.env
-cd /root/TeamUltroid
+cp ultroid/.env /root/NS-ANONYMUS/.env
+cd /root/NS-ANONYMUS
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
 docker run --privileged --env-file .env --rm -i ultroid
